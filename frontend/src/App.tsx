ca04df8fc7ba2fd760faa60c16b4f8e66c91236a
Bruno <brunocamposo.r@gmail.com>
2026-04-21 06:00:03 -0300

    Adicionando side bar e ajustando a responsividade

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,10 @@
 import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import {Signup} from "./pages/Signup";
-import {Login} from "./pages/Login";
-import {Home} from "./pages/Home";
-import {ForgotPassword} from "./pages/ForgotPassword";
-import {Dashboard} from './pages/Dashboard';
+import { Signup } from "./pages/Signup";
+import { Login } from "./pages/Login";
+import { Home } from "./pages/Home";
+import { ForgotPassword } from "./pages/ForgotPassword";
+import { Dashboard } from './pages/Dashboard';
 
 function App() {
   return (
@@ -12,9 +12,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/signup" element= {<Signup />}/>
-        <Route path="/forgot-password" element= {<ForgotPassword />}/>
-        <Route path="/dashboard" element= {<Dashboard />}/>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend/src/components/AppSidebar.tsx
@@ -0,0 +1,111 @@
+"use client"
+
+import * as React from "react"
+import { ArchiveX, Command, File, Inbox, Send, Trash2 } from "lucide-react"
+import { NavUser } from "./NavUser"
+import { Label } from "./ui/label"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarHeader,
+  SidebarInput,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  useSidebar,
+} from "./ui/sidebar"
+import { Switch } from "./ui/switch"
+
+// This is sample data
+const data = {
+  user: {
+    name: "shadcn",
+    email: "m@example.com",
+    avatar: "/avatars/shadcn.jpg",
+  },
+  navMain: [
+    {
+      title: "Inbox",
+      url: "#",
+      icon: Inbox,
+      isActive: true,
+    },
+    {
+      title: "Drafts",
+      url: "#",
+      icon: File,
+      isActive: false,
+    },
+    {
+      title: "Sent",
+      url: "#",
+      icon: Send,
+      isActive: false,
+    },
+    {
+      title: "Junk",
+      url: "#",
+      icon: ArchiveX,
+      isActive: false,
+    },
+    {
+      title: "Trash",
+      url: "#",
+      icon: Trash2,
+      isActive: false,
+    },
+  ],
+}
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  const [activeItem, setActiveItem] = React.useState(data.navMain[0])
+
+  return (
+    <Sidebar
+      collapsible="icon"
+      {...props}
+    >
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton size="lg" asChild>
+              <a href="#" className="flex items-center gap-3">
+                <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground shrink-0">
+                  <Command className="size-4" />
+                </div>
+                <div className="grid flex-1 text-left text-sm leading-tight">
+                  <span className="truncate font-medium">Acme Inc</span>
+                </div>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarMenu>
+            {data.navMain.map((item) => (
+              <SidebarMenuItem key={item.title}>
+                <SidebarMenuButton
+                  tooltip={item.title}
+                  isActive={activeItem?.title === item.title}
+                  onClick={() => setActiveItem(item)}
+                  className="gap-3" // Ajusta a distância entre ícone e texto
+                >
+                  <item.icon className="shrink-0" />
+                  <span>{item.title}</span>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            ))}
+          </SidebarMenu>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+    </Sidebar>
+  )
+}
