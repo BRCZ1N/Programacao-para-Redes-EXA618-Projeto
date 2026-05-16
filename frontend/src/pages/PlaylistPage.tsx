76cba535bd3402eadf4f1841c006e391367aab04
Bruno <brunocamposo.r@gmail.com>
2026-05-16 16:47:50 -0300

    Ajustando bugs

--- frontend/src/pages/PlaylistPage.tsx
+++ frontend/src/pages/PlaylistPage.tsx
@@ -41,7 +41,7 @@ export function PlaylistPage() {
 
     const fetchPlaylist = async () => {
       try {
-        const res = await fetch(`https://programacao-para-redes-exa618-projeto.onrender.com/api/playlist/${id}`, {
+        const res = await fetch(`https://programacao-para-redes-exa618-projeto.onrender.com/api/playlist/${id}/`, {
           credentials: "include",
         });
 
