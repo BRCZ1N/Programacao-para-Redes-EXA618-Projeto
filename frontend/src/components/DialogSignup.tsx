b28dae51152b254a8ed757f1013401e6bf3b903e
Bruno <brunocamposo.r@gmail.com>
2026-05-27 21:27:13 -0300

    Update DialogSignup.tsx

--- frontend/src/components/DialogSignup.tsx
+++ frontend/src/components/DialogSignup.tsx
@@ -68,7 +68,6 @@ export function DialogSignup({ open, onOpenChange, onGoToLogin }: Props) {
       } else {
         const data = await res.json();
         setError(Object.values(data)[0] as string[][0]);
-        console.log(Object.values(data)[0] as string[][0]);
       }
     } catch {
       setError("Erro de conexão");
