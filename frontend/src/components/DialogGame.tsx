abaaaac85b0c8671ee9e0c100bb549ebd17e1a75
Bruno <brunocamposo.r@gmail.com>
2026-05-28 03:33:03 -0300

    Update DialogGame.tsx

--- frontend/src/components/DialogGame.tsx
+++ frontend/src/components/DialogGame.tsx
@@ -42,7 +42,7 @@ export function DialogGame({
     async function loadGame() {
       try {
         const res = await fetch(
-          `https://programacao-para-redes-exa618-projeto.onrender.com/api/game/${gameId}/`,
+          `https://programacao-para-redes-exa618-projeto.onrender.com/api/games/${gameId}/`,
           {
             credentials: "include",
           }
