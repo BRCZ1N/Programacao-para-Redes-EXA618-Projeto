c715e0754867bae313fae4eda1d1242b54f75f63
Bruno <brunocamposo.r@gmail.com>
2026-05-28 00:28:47 -0300

    Testando sem privação de rotas

--- frontend/src/utils/PrivateRoutes.tsx
+++ frontend/src/utils/PrivateRoutes.tsx
@@ -1,18 +1,9 @@
 import {Outlet, useNavigate } from "react-router-dom";
 import { useAuth } from "./AuthProvider";
-import { Spinner } from "../components/ui/spinner";
 
 export function PrivateRoutes() {
-  const { user, loading } = useAuth();
+  const { user} = useAuth();
   const navigate = useNavigate();
-
-  if (loading && !user) {
-    return (
-      <div className="flex items-center justify-center w-full h-screen bg-black">
-        <Spinner className="size-8 text-white" />
-      </div>
-    );
-  }
 
   if (!user) {
     navigate("/");
