aaf8cba19d299febc4f483b794c0df78e7ec6acd
Bruno <brunocamposo.r@gmail.com>
2026-05-28 01:39:09 -0300

    Update AppSidebar.tsx

--- frontend/src/components/AppSidebar.tsx
+++ frontend/src/components/AppSidebar.tsx
@@ -25,7 +25,7 @@ export function AppSidebar() {
   const [search, setSearch] = useState("");
 
   const [nextUrl, setNextUrl] = useState<string | null>(
-    "https://programacao-para-redes-exa618-proje.vercel.app/api/playlist/"
+    "https://programacao-para-redes-exa618-projeto.onrender.com/api/playlist/"
   );
 
   const isFetchingRef = useRef(false);
